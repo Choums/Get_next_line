334599f6d726fc3fa30cc15f995afd5722598be5
Chams AIDEL <chaidel@e1r8p2.42.fr>
2021-12-16 05:26:04 +0100

    strjoin protected

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: chaidel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 16:47:18 by chaidel           #+#    #+#             */
-/*   Updated: 2021/12/04 16:41:57 by chaidel          ###   ########.fr       */
+/*   Updated: 2021/12/16 05:23:14 by chaidel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,17 @@ char	*ft_strjoin(char *rest, char const *brut)
 	if (!rest)
 	{
 		rest = (char *)malloc(sizeof(char));
+		if (!rest)
+			return (NULL);
 		rest[0] = '\0';
 	}
-	if (!brut || !rest)
+	if (!brut)
 		return (NULL);
 	one = malloc(sizeof(char) * (ft_strlen(rest) + ft_strlen(brut) + 1));
 	if (!one)
 		return (NULL);
-	if (rest)
-		while (rest[++i])
-			one[i] = rest[i];
+	while (rest[++i])
+		one[i] = rest[i];
 	while (brut[j])
 		one[i++] = brut[j++];
 	one[i] = '\0';
